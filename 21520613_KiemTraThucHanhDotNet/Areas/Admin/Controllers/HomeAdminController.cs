5fe1aa4ffbae151777d673b6b526165c5155b1c6
QuocBaoKho <baonhq2603@gmail.com>
2024-11-04 23:52:16 +0700

    Add new features for Admin

--- 21520613_KiemTraThucHanhDotNet/Areas/Admin/Controllers/HomeAdminController.cs
+++ 21520613_KiemTraThucHanhDotNet/Areas/Admin/Controllers/HomeAdminController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity.Client;
 
 namespace _21520613_KiemTraThucHanhDotNet.Areas.Admin.Controllers
 {
@@ -48,5 +49,55 @@ namespace _21520613_KiemTraThucHanhDotNet.Areas.Admin.Controllers
             }
             return View(dm);
         }
+        [Route("edit")]
+        [HttpGet]
+        public IActionResult Edit(string id)
+        {
+            ViewBag.MaChatLieu = new SelectList(db.TChatLieus, "MaChatLieu", "ChatLieu");
+            ViewBag.MaHangSx = new SelectList(db.THangSxes, "MaHangSx", "HangSx");
+            ViewBag.MaNuocSx = new SelectList(db.TQuocGia, "MaNuoc", "TenNuoc");
+            ViewBag.MaLoai = new SelectList(db.TLoaiSps, "MaLoai", "Loai");
+            ViewBag.MaDt = new SelectList(db.TLoaiDts, "MaDt", "TenLoai");
+            var updated = db.TDanhMucSps.Find(id);
+            return View(updated);
+        }
+        [Route("edit")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(TDanhMucSp dm)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(dm).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Manage_Product");
+            }
+            return View(dm);
+        }
+        [HttpGet]
+        public IActionResult Delete(string id)
+        {
+            TempData["message"] = "Chua xoa";
+            var ctSp = db.TChiTietSanPhams.Where(ct=>ct.MaSp.Equals(id)).ToList();
+            var tenSp = db.TDanhMucSps.Where(ct => ct.MaSp.Equals(id)).Select(ct => ct.TenSp).FirstOrDefault();
+            if (ctSp.Count > 0)
+            {
+                TempData["message"] = "Can't remove " + tenSp;
+                return RedirectToAction("Manage_Product", "homeadmin");
+            }
+            var listHinhAnh = db.TAnhSps.Where(anh=>anh.MaSp.Equals(id)).ToList();
+            if (listHinhAnh.Any())
+            {
+                db.RemoveRange(listHinhAnh);
+            }
+            db.Remove(db.TDanhMucSps.Find(id));
+            db.SaveChanges();
+            TempData["message"] = tenSp + " Successfully removed";
+            return RedirectToAction("Manage_Product", "homeadmin");
+
+
+
+        }
+
     }
 }
